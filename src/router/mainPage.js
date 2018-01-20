95799560e1518f1d317def2950d84bfd46dd91f8
zhaogang <zgyxx091418@163.com>
2018-01-21 00:15:51 +0800

    路由配置

--- src/router/mainPage.js
+++ src/router/mainPage.js
@@ -2,8 +2,14 @@
  * Created by zhuliang on 2017/12/25.
  */
 const mainPage = resolve => require(['@/components/mainPage/mainPage.vue'], resolve);
+
 const colorSizeManage = resolve => require(['@/components/system/colorSizeManage.vue'], resolve);
+
 const systemSetting = resolve => require(['@/components/system/systemSetting.vue'], resolve);
+
+const repertoryRecord = resolve => require(['@/components/repertory/repertoryRecord.vue'], resolve);
+//销售开单
+const salesOrder = resolve => require(['@/components/goods/salesOrder.vue'], resolve);
 
 const mainPageRouter = {
   path: '/',
@@ -11,11 +17,29 @@ const mainPageRouter = {
   children: [
     {
       path: '/colorSizeManage',
-      component: colorSizeManage
+      component: colorSizeManage,
+      name:"尺码颜色管理",
+      //needAlive表示该页面是否是标签页,keepAlive表示是否需要缓存改页面
+      meta:{ needAlive:true,keepAlive:true },
     },
     {
       path: '/systemSetting',
-      component: systemSetting
+      component: systemSetting,
+      name:"系统设置",
+      meta:{ needAlive:true,keepAlive:true },
+    },
+    {
+      path: '/repertoryRecord',
+      component: repertoryRecord,
+      name:"库存管理",
+      //needAlive表示该页面是否是标签页,keepAlive表示是否需要缓存改页面
+      meta:{ needAlive:true,keepAlive:true },
+    },
+    {
+      path: '/salesOrder',
+      component: salesOrder,
+      name:"销售开单",
+      meta:{ needAlive:true,keepAlive:true },
     },
   ]
 };
