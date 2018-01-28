de96b74b8576c0a73489cec45f5bb662dce45423
zhaogang <zgyxx091418@163.com>
2018-01-28 20:06:38 +0800

    调货

---
+++ src/components/repertory/seasoningExpand.vue
@@ -0,0 +1,235 @@
+<template>
+  <div class="good-table-expand">
+    <!-- <Table class="my-table" :show-header="!showHeader"  :columns="columns1" :data="data1"></Table>-->
+    <Card v-for="item in data1" @click.native="returnItem(item)">
+      <div class="good-card">
+        <div class="left-content">
+          <img class="good-img" :src="item.img">
+        </div>
+        <div class="center-content">
+          <div class="top-content">
+            <div>商品货号:{{item.id}}</div>
+            <div>商品名称:{{item.name}}</div>
+          </div>
+          <div class="bottom-content">
+            <div class="color-content">
+              <div class="colordiamonds" :style="{background:item.colorName}"></div>
+              <div class="colorName">{{item.color}}</div>
+            </div>
+            <div class="size-content">
+              {{item.size}}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      row: Object
+    },
+    data() {
+      return {
+        showHeader: false,
+        columns1: [
+          {
+            align: 'center',
+            title: '图片',
+            key: 'img',
+            render: (h, params) => {
+              return h('img', {
+                style: {
+                  maxWidth: '80px',
+                  maxHeight: '80px'
+
+                },
+                domProps: {
+                  src: params.row.img
+                },
+              });
+            }
+          }, {
+            title: '货号',
+            key: 'id'
+          }, {
+            title: '颜色',
+            key: 'color',
+            render: (h, params) => {
+              return h('div', [
+                h('div', {
+                  style: {
+                    background: params.row.colorName,
+                    height: '1em',
+                    width: '1em',
+                    display: 'inline-block',
+                  },
+                }, ''),
+                h('div', {
+                  style: {
+                    color: params.row.colorName,
+                    display: 'inline-block',
+                    position: 'relative',
+                    top: '-2px',
+                    marginLeft: '5px',
+                  },
+                }, params.row.color)
+              ]);
+            }
+
+          }, {
+            title: '尺码',
+            key: 'size'
+          }, {
+            title: '单价',
+            key: 'prince'
+          }, {
+            title: '数量',
+            key: 'count',
+            render: (h, params) => {
+              return h('div', [
+                h('p', {
+                  style: {
+                    color: '#c1c2c3'
+                  },
+                }, 'x' + params.row.count),
+                h('p', {
+                  style: {
+                    color: '#fc934f'
+                  },
+                }, parseInt(params.row.returnCount, 10) > 0 ? '-' + params.row.returnCount : '')
+              ]);
+            }
+          },
+        ],
+        data1: [
+          {
+            img: 'https://img.alicdn.com/bao/uploaded/i2/1811379809/TB1qpkvlh3IL1JjSZPfXXcrUVXa_!!0-item_pic.jpg_430x430q90.jpg',
+            id: '892726252525/0905',
+            color: '浅蓝色',
+            colorName: '#65cefa',
+            size: 'L',
+            prince: 152.00,
+            count: 10000,
+            returnCount: 1,
+            name: '01016下口袋'
+          }
+        ]
+      }
+    },
+    components: {},
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+      returnItem(item) {
+        this.$emit('returnItem', item)
+      }
+    }
+  }
+</script>
+<style lang="scss" rel="stylesheet/scss" type="text/scss">
+  @import "../../common/css/globalscss";
+
+  .good-table-expand {
+    display: flex;
+    width: 100%;
+    position: relative;
+    flex-wrap: wrap;
+    .expand-row {
+      margin-bottom: 16px;
+
+    }
+    .ivu-card {
+      overflow: hidden;
+      margin-top: 1%;
+      width: 49%;
+      flex: 1 1 49%;
+      position: relative;
+    }
+    .ivu-card:nth-child(2n) {
+      margin-left: 1%;
+    }
+    .good-card {
+      position: relative;
+      display: flex;
+      .left-content, .center-content {
+        flex: 1 1 40%;
+      }
+      .right-content {
+        flex: 1 1 20%;
+      }
+      .good-img {
+        width: 85px;
+        height: 85px;
+      }
+      .center-content {
+        font-size: 14px;
+        .bottom-content {
+          display: flex;
+          .size-content, .color-content {
+            width: 65px;
+            height: 25px;
+            padding: 3px;
+            border: 1px solid $menuSelectFontColor;
+            font-size: 12px;
+            margin-top: 3px;
+            border-radius: 3px;
+          }
+          .color-content {
+            display: flex;
+            color: $menuSelectFontColor;
+            .colordiamonds {
+              margin-top: 2.5px;
+              width: 12px;
+              height: 12px;
+              margin-right: 3px;
+              margin-left: 2px;
+            }
+          }
+          .size-content {
+            margin-left: 2px;
+            background: $menuSelectFontColor;
+            color: #fff;
+            text-align: center;
+          }
+        }
+      }
+      .center-content, .right-content {
+        line-height: 140%;
+      }
+      .right-content {
+        font-size: 16px;
+        text-align: right;
+        .count-content {
+          color: #c1c2c3;
+        }
+        .returnCount {
+          color: #ff7300;
+        }
+      }
+    }
+
+  }
+
+  @media (max-width: 920px) {
+    .good-table-expand {
+      display: block;
+      .ivu-card {
+        margin-left: 0% !important;
+        min-width: 100%;
+        position: relative;
+        .good-card {
+          .left-content {
+            text-align: center;
+          }
+        }
+      }
+    }
+  }
+
+</style>
