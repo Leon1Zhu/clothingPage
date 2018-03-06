f499f948eb28d26309bc95f900841a2a32adb732
zhaogang <zgyxx091418@163.com>
2018-03-06 12:40:18 +0800

    颜色选择和尺码选择的组件封装

---
+++ src/common/vue/colorChoose.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="">
+    <Modal
+      v-model="colorOpen"
+      title="颜色选择"
+      @on-ok="ok"
+      @on-cancel="cancel">
+      <Card v-for="colorData in colorDataS" class="card-color">
+        <p slot="title" class="title">
+          {{colorData.seriesName}}&nbsp;&nbsp;
+        </p>
+        <Tag :class="{'active': color.isActive ==='add'}" v-for="color in colorData.colors" type="dot"
+             :color="color.colorRgb" @click.native="selectColor(color)">
+          {{color.colorName}}
+        </Tag>
+      </Card>
+    </Modal>
+  </div>
+</template>
+<script>
+  import Vue from 'vue';
+  import colorApi from '../../api/colorManage';
+
+  export default {
+    props: {
+      colorOpen: {
+        default: false
+      },
+      selectColorArray: {
+        type: Array
+      }
+    },
+    data() {
+      return {
+        colorDataS: []
+      };
+    },
+    created() {
+      this.getSysTemColor();
+      for (let colorSelect of this.selectColorArray) {
+        for (let colors of this.colorDataS) {
+          for (let singleColor of colors) {
+            if (singleColor.colorId === colorSelect.colorId) {
+              Vue.set(singleColor, 'isActive', 'add');
+            }
+          }
+        }
+      }
+    },
+    methods: {
+      getSysTemColor() {
+        colorApi.getSysTemColor(this.$store.getters.getAccountId).then((response) => {
+          this.colorDataS = response.data;
+        }).catch((error) => {
+          this.$error(apiError, '获取错误')
+        });
+      },
+      selectColor(color) {
+        if (color.isActive === '' || color.isActive === undefined || color.isActive === 'delete') {
+          Vue.set(color, 'isActive', 'add');
+        } else {
+          Vue.set(color, 'isActive', 'delete');
+        }
+        this.$emit('select-color', color);
+      },
+      cancel() {
+        this.colorOpen = false;
+      },
+      ok() {
+        this.colorOpen = false;
+      }
+    },
+    components: {}
+  };
+</script>
+<style lang="scss" rel="stylesheet/scss" type="text/scss">
+  .card-color {
+    margin-top: 8px;
+    .active {
+      border: 1px solid #06b9a5 !important;
+    }
+    .ui-cursor {
+      font-weight: 600;
+    }
+  }
+</style>
---
+++ src/common/vue/sizeChoose.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="">
+    <Modal
+      v-model="sizeOpen"
+      title="尺码选择"
+      @on-ok="ok"
+      @on-cancel="cancel">
+      <Card v-for="sizeData in sizeDataS" class="card-size">
+        <p slot="title" class="title">
+          {{sizeData.sizetypeName}}
+        </p>
+        <Tag v-for="size in sizeData.sizes" :class="{'active': size.isActive ==='add'}"
+             @click.native="selectSize(size)" type="dot" class="size-tag"
+             color="#06c1ae">
+          {{size.sizeName}}
+        </Tag>
+      </Card>
+    </Modal>
+  </div>
+</template>
+<script>
+  import Vue from 'vue';
+  import sizeApi from '../../api/sizeManage';
+
+  export default {
+    props: {
+      sizeOpen: {
+        default: false
+      },
+      selectSizeArray: {
+        type: Array
+      }
+    },
+    data() {
+      return {
+        sizeDataS: []
+      };
+    },
+    created() {
+      this.getSysTemSize();
+      for (let sizeSelect of this.selectSizeArray) {
+        for (let sizes of this.sizeDataS) {
+          for (let singleSize of sizes) {
+            if (singleSize.sizeId === sizeSelect.sizeId) {
+              Vue.set(singleSize, 'isActive', 'add');
+            }
+          }
+        }
+      }
+    },
+    methods: {
+      getSysTemSize() {
+        sizeApi.getSysTemSize(this.$store.getters.getAccountId).then((response) => {
+          this.sizeDataS = response.data;
+        }).catch((error) => {
+          this.$error(apiError, '获取错误')
+        });
+      },
+      selectSize(size) {
+        if (size.isActive === '' || size.isActive === undefined || size.isActive === 'delete') {
+          Vue.set(size, 'isActive', 'add');
+        } else {
+          Vue.set(size, 'isActive', 'delete');
+        }
+        this.$emit('select-size', size);
+      },
+      cancel() {
+        this.sizeOpen = false;
+      },
+      ok() {
+        this.sizeOpen = false;
+      }
+    },
+    components: {}
+  };
+</script>
+<style lang="scss" rel="stylesheet/scss" type="text/scss">
+  .card-size {
+    margin-top: 8px;
+    .active {
+      border: 1px solid #06b9a5 !important;
+    }
+    .size-tag {
+      width: 95px !important;
+      text-align: center;
+    }
+    .ui-cursor {
+      font-weight: 600;
+    }
+  }
+</style>
